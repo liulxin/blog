741acaa28bb5fa5352c07aac3d7ed171d5194841
dongyuanjie.wudi <dongyuanjie.wudi@shengqugames.com>
2019-12-02 17:43:36 +0800

    vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,23 @@
 module.exports = {
-  title: 'hello vuepress',
-  description: 'just playing around'
-}
+  title: "liulxin",
+  description: "code learning",
+  themeConfig: {
+    sidebar: "auto",
+    repo: "https://github.com/liulxin",
+    smoothScroll: true,
+    nav: require("./nav/zh"),
+    collapsable: false,
+    sidebarDepth: 2,
+    displayAllHeaders: true,
+    lastUpdated: "Last Updated",
+    sidebar: {
+      "/js/": [
+        {
+          title: "JavaScript",
+          collapsable: false,
+          children: ["", "test"]
+        }
+      ]
+    }
+  }
+};
